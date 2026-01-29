d8a16acfe1622a42f7eba59248a7e4663406f378
Lenovictor <lenovictor145@gmail.com>
2026-01-29 09:17:27 +0100

    Change API base URL to production server

--- frontend/src/axiosConfig.js
+++ frontend/src/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: "https://liste-react-mongodb-backend.onrender.com",
   withCredentials: true,
 });
 
