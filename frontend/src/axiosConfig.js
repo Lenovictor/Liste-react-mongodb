e67ee1d6bc983a13200da96149a5ec126337e1ea
Lenovictor <lenovictor145@gmail.com>
2026-01-29 10:26:19 +0100

    Update base URL in axios configuration

--- frontend/src/axiosConfig.js
+++ frontend/src/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://liste-react-mongodb-backend.onrender.com",
+  baseURL: "https://liste-react-mongodb.onrender.com/",
   withCredentials: true,
 });
 
